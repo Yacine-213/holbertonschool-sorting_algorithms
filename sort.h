b5b2793a4478170c86e0afa86172dec604b977fe
Yacine-213 <d.yacine2020@gmail.com>
2025-01-13 10:25:34 +0000

    0

---
+++ sort.h
@@ -0,0 +1,13 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>  // for size_t
+
+/* Function prototype for bubble_sort */
+void bubble_sort(int *array, size_t size);
+
+/* Function prototype for print_array (for displaying the array) */
+void print_array(const int *array, size_t size);
+
+#endif /* SORT_H */
+
